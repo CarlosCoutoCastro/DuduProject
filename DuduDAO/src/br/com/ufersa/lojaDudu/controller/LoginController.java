43bc8d33121f8ede0ad6f106ecc213d1eafd2d50
ThallissonDouglas <thallisson.nunes@alunos.ufersa.edu.br>
2023-09-26 23:19:16 -0300

    view e controller
    - view e controller iniciados, mas não finalizados.
    
    - organização das pastas do projeto.

---
+++ DuduDAO/src/br/com/ufersa/lojaDudu/controller/LoginController.java
@@ -0,0 +1,14 @@
+package br.com.ufersa.lojaDudu.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML private TextField usuario;
+    @FXML private TextField senha;
+
+    public void autenticar(ActionEvent event) {
+
+    }
+}
---
+++ DuduDAO/src/br/com/ufersa/lojaDudu/view/TelaVenda.java
@@ -0,0 +1,17 @@
+package br.com.ufersa.lojaDudu.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TelaVenda extends Application {
+    public static void main(String[] args) throws Exception {
+        launch();
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+    
+}
